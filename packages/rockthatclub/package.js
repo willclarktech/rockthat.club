35915f79c531336ef2f9ca7e91c8c9a44732be7c
willclarktech <will@willclark.tech>
2017-06-24 19:42:43 +0200

    remove example forum from package

--- packages/rockthatclub/package.js
+++ packages/rockthatclub/package.js
@@ -1,11 +1,13 @@
 Package.describe({
   name: 'rockthatclub',
+  summary: 'finde deinen Gig',
+  version: '0.0.1',
+  git: "https://github.com/willclarktech/rockthat.club.git"
 })
 
 Package.onUse(function(api) {
   api.use([
     'vulcan:core',
-    'example-forum',
   ])
 
   api.mainModule('lib/server/main.js', 'server')
