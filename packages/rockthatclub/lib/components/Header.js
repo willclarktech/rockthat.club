b0ec596a1c8254a2e042807b6909249575b366e3
willclarktech <will@willclark.tech>
2017-06-26 12:28:56 +0200

    add custom Header component

---
+++ packages/rockthatclub/lib/components/Header.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { withCurrentUser, getSetting, Components, registerComponent } from 'meteor/vulcan:core'
+
+const LOGO_URL = getSetting('logoUrl')
+const SITE_TITLE = getSetting('title', 'My App')
+const TAGLINE = getSetting('tagline')
+
+const Header = ({
+  currentUser,
+}) => (
+  <div className="header-wrapper">
+
+    <header className="header">
+
+      <div className="logo">
+        <Components.Logo logoUrl={LOGO_URL} siteTitle={SITE_TITLE} />
+        {TAGLINE ? <h2 className="tagline">{TAGLINE}</h2> : null }
+      </div>
+
+      <div className="nav">
+
+        <div className="nav-user">
+          {!!currentUser ? <Components.UsersMenu /> : <Components.UsersAccountMenu />}
+        </div>
+
+        {/* <div className="nav-new-post">
+          <Components.PostsNewButton/>
+        </div> */}
+
+      </div>
+
+    </header>
+  </div>
+)
+
+Header.propTypes = {
+  currentUser: PropTypes.object,
+}
+
+registerComponent('Header', Header, withCurrentUser)
--- packages/rockthatclub/lib/components/index.js
+++ packages/rockthatclub/lib/components/index.js
@@ -1,3 +1,4 @@
+import './Header'
 import './Footer'
 import './Layout'
 
