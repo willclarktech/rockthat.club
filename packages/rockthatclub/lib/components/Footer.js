642ccdb3d6e31f523a6c15ab123047a9be0c9e3d
willclarktech <willclarktech@users.noreply.github.com>
2017-07-09 17:43:15 +0200

    add email to Footer

--- packages/rockthatclub/lib/components/Footer.js
+++ packages/rockthatclub/lib/components/Footer.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import { replaceComponent } from 'meteor/vulcan:core'
 
+const email = 'wadim@rockthat.club'
+const href = `mailto:${email}`
+
 const Footer = () => (
   <div className="footer">
-    Something for the footer
+    See an artist you like? Email <a href={href}>{email}</a> and we’ll put you in touch.
   </div>
 )
 
