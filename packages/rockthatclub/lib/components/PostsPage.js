ede8150537d8dd5ce0a0964ef368fffd54babca2
willclarktech <willclarktech@users.noreply.github.com>
2017-07-09 16:35:44 +0200

    add custom PostsPage component

---
+++ packages/rockthatclub/lib/components/PostsPage.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Components, getRawComponent, replaceComponent } from 'meteor/vulcan:core'
+import { FormattedMessage } from 'meteor/vulcan:i18n';
+import Posts from '../modules/posts/collection'
+
+class PostsPage extends getRawComponent('PostsItem') {
+  render() {
+    if (this.props.loading) {
+
+      return <div className="posts-page"><Components.Loading/></div>
+
+    } else if (!this.props.document) {
+      return <div className="posts-page"><FormattedMessage id="app.404"/></div>
+
+    } else {
+      const post = this.props.document;
+
+      const htmlBody = {__html: post.htmlBody};
+
+      return (
+        <div className="posts-page">
+          <Components.HeadTags url={Posts.getPageUrl(post, true)} title={post.title} image={post.thumbnailUrl} description={post.excerpt} />
+
+          <Components.PostsItem post={post} currentUser={this.props.currentUser} />
+          { post.soundcloud }
+          {post.htmlBody ? <div className="posts-page-body" dangerouslySetInnerHTML={htmlBody}></div> : null}
+
+          <Components.PostsCommentsThread terms={{postId: post._id, view: 'postComments'}} />
+
+        </div>
+      )
+    }
+  }
+}
+
+replaceComponent('PostsPage', PostsPage)
