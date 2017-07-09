045e5b9fdbbc212bd736137fcd710e0541875ca5
willclarktech <willclarktech@users.noreply.github.com>
2017-07-09 17:36:38 +0200

    refactor custom Post fields

--- packages/rockthatclub/lib/modules/posts/collection.js
+++ packages/rockthatclub/lib/modules/posts/collection.js
@@ -2,40 +2,41 @@ import Posts from 'meteor/vulcan:posts'
 import './fragments'
 import './i18n/en_US'
 
-Posts.addField({
-  fieldName: 'soundcloud',
-  fieldSchema: {
-    type: String,
-    control: 'text',
-    optional: true,
-    insertableBy: ['admins'],
-    editableBy: ['admins'],
-    viewableBy: ['admins'],
-  },
-})
 
-Posts.addField({
-  fieldName: 'canBring',
-  fieldSchema: {
-    type: Number,
-    control: 'number',
-    optional: true,
-    insertableBy: ['admins'],
-    editableBy: ['admins'],
-    viewableBy: ['admins'],
+Posts.addField([
+  {
+    fieldName: 'soundcloud',
+    fieldSchema: {
+      type: String,
+      control: 'text',
+      optional: true,
+      insertableBy: ['admins'],
+      editableBy: ['admins'],
+      viewableBy: ['members'],
+    },
   },
-})
-
-Posts.addField({
-  fieldName: 'minimumCharge',
-  fieldSchema: {
-    type: Number,
-    control: 'number',
-    optional: true,
-    insertableBy: ['admins'],
-    editableBy: ['admins'],
-    viewableBy: ['admins'],
+  {
+    fieldName: 'canBring',
+    fieldSchema: {
+      type: Number,
+      control: 'number',
+      optional: true,
+      insertableBy: ['admins'],
+      editableBy: ['admins'],
+      viewableBy: ['members'],
+    },
   },
-})
+  {
+    fieldName: 'minimumCharge',
+    fieldSchema: {
+      type: Number,
+      control: 'number',
+      optional: true,
+      insertableBy: ['admins'],
+      editableBy: ['admins'],
+      viewableBy: ['members'],
+    },
+  },
+])
 
 export default Posts
