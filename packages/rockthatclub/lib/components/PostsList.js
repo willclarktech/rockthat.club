dcd05ff6917b499004bc647432f2aec832dbdaab
willclarktech <willclarktech@users.noreply.github.com>
2017-07-09 14:04:21 +0200

    copy PostsList component with custom fragment

--- packages/rockthatclub/lib/components/PostsList.js
+++ packages/rockthatclub/lib/components/PostsList.js
@@ -1,29 +1,80 @@
-import React from 'react'
-import { Components, withCurrentUser, withList } from 'meteor/vulcan:core'
-import Posts from '../modules/posts/collection'
+import { Components, replaceComponent, withList, withCurrentUser, Utils } from 'meteor/vulcan:core';
+import React from 'react';
+import PropTypes from 'prop-types';
+import Alert from 'react-bootstrap/lib/Alert'
+import { FormattedMessage, intlShape } from 'meteor/vulcan:i18n';
+import classNames from 'classnames';
+import Posts from '../modules/posts/collection';
 
-const PostsList = ({
-  currentUser,
-  results = [],
-  loading,
-}) => (
-  <div>
-    <Components.PostsListHeader />
-    <div className="posts-list-content">
-      {results.map(post => (
-        <Components.PostsItem
-          key={post._id}
-          post={post}
-        />
-      ))}
-    </div>
-  </div>
-)
+const Error = ({error}) => <Alert className="flash-message" bsStyle="danger"><FormattedMessage id={error.id} values={{value: error.value}}/>{error.message}</Alert>
+
+const PostsList = ({className, results, loading, count, totalCount, loadMore, showHeader = true, showLoadMore = true, networkStatus, currentUser, error, terms}) => {
+
+  const loadingMore = networkStatus === 2;
+
+  if (results && results.length) {
+    const hasMore = totalCount > results.length;
+
+    return (
+      <div className={classNames(className, 'posts-list')}>
+        {showHeader ? <Components.PostsListHeader/> : null}
+        {error ? <Error error={Utils.decodeIntlError(error)} /> : null }
+        <div className="posts-list-content">
+          {results.map(post => <Components.PostsItem post={post} key={post._id} currentUser={currentUser} terms={terms} />)}
+        </div>
+        {showLoadMore ?
+          hasMore ?
+            <Components.PostsLoadMore loading={loadingMore} loadMore={loadMore} count={count} totalCount={totalCount} /> :
+            <Components.PostsNoMore/> :
+          null
+        }
+      </div>
+    )
+  } else if (loading) {
+    return (
+      <div className={classNames(className, 'posts-list')}>
+        {showHeader ? <Components.PostsListHeader /> : null}
+        {error ? <Error error={Utils.decodeIntlError(error)} /> : null }
+        <div className="posts-list-content">
+          <Components.PostsLoading/>
+        </div>
+      </div>
+    )
+  } else {
+    return (
+      <div className={classNames(className, 'posts-list')}>
+        {showHeader ? <Components.PostsListHeader /> : null}
+        {error ? <Error error={Utils.decodeIntlError(error)} /> : null }
+        <div className="posts-list-content">
+          <Components.PostsNoResults/>
+        </div>
+      </div>
+    )
+  }
+
+};
+
+PostsList.displayName = "PostsList";
+
+PostsList.propTypes = {
+  results: PropTypes.array,
+  terms: PropTypes.object,
+  hasMore: PropTypes.bool,
+  loading: PropTypes.bool,
+  count: PropTypes.number,
+  totalCount: PropTypes.number,
+  loadMore: PropTypes.func,
+  showHeader: PropTypes.bool,
+};
+
+PostsList.contextTypes = {
+  intl: intlShape
+};
 
 const options = {
   collection: Posts,
-  fragmentName: 'PostsItemFragment',
-  limit: 5,
-}
+  queryName: 'postsListQuery',
+  fragmentName: 'PostsListCustom',
+};
 
 export default withList(options)(withCurrentUser(PostsList))
--- packages/rockthatclub/lib/modules/posts/fragments.js
+++ packages/rockthatclub/lib/modules/posts/fragments.js
@@ -5,7 +5,58 @@ registerFragment(`
     _id
     title
     postedAt
-    url
     slug
   }
 `)
+
+registerFragment(`
+  fragment PostsListCustom on Post {
+    # vulcan:posts
+    _id
+    title
+    url
+    slug
+    postedAt
+    createdAt
+    sticky
+    status
+    body
+    htmlBody
+    excerpt
+    viewCount
+    clickCount
+    # vulcan:users
+    userId
+    user {
+      ...UsersMinimumInfo
+    }
+    # vulcan:embedly
+    # thumbnailUrl
+    # vulcan:categories
+    categories {
+      ...CategoriesMinimumInfo
+    }
+    # vulcan:comments
+    commentCount
+    commenters {
+      ...UsersMinimumInfo
+    }
+    # vulcan:voting
+    upvoters {
+      _id
+    }
+    downvoters {
+      _id
+    }
+    upvotes
+    downvotes
+    baseScore
+    score
+  }
+`);
+
+registerFragment(`
+  fragment PostsPage on Post {
+    ...PostsListCustom
+  }
+`);
