784cb673ad2e50e99910c1436b96bd1cdd94aee3
willclarktech <willclarktech@users.noreply.github.com>
2017-07-09 15:58:32 +0200

    add custom test for new post

--- packages/rockthatclub/lib/modules/posts/collection.js
+++ packages/rockthatclub/lib/modules/posts/collection.js
@@ -1,4 +1,5 @@
 import Posts from 'meteor/vulcan:posts'
 import './fragments'
+import './i18n/en_US'
 
 export default Posts
---
+++ packages/rockthatclub/lib/modules/posts/i18n/en_US.js
@@ -0,0 +1,5 @@
+import { addStrings } from 'meteor/vulcan:core';
+
+addStrings('en', {
+  "posts.new_post": "New Artist Post",
+});
