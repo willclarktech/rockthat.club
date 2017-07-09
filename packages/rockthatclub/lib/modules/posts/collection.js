715a33c678306c3db2a3e324a6297e8e23c0385a
willclarktech <willclarktech@users.noreply.github.com>
2017-07-09 16:35:58 +0200

    add soundcloud field to Post

--- packages/rockthatclub/lib/modules/posts/collection.js
+++ packages/rockthatclub/lib/modules/posts/collection.js
@@ -2,4 +2,17 @@ import Posts from 'meteor/vulcan:posts'
 // import './fragments'
 import './i18n/en_US'
 
+Posts.addField(
+  {
+    fieldName: 'soundcloud',
+    fieldSchema: {
+      type: String,
+      control: 'text', // use a select form control
+      optional: true, // this field is not required
+      insertableBy: ['admins'],
+      editableBy: ['admins'],
+      viewableBy: ['admins'],
+    }
+  })
+
 export default Posts
--- packages/rockthatclub/lib/server/seed.js
+++ packages/rockthatclub/lib/server/seed.js
@@ -7,16 +7,19 @@ const seedData = [
   {
     title: 'Led Zeppelin',
     thumbnailUrl: 'https://pbs.twimg.com/profile_images/2585954566/8ev9krfvkxb8nz00cg77_400x400.jpeg',
+    soundcloud: '88796626',
     // url: 'http://www.ledzeppelin.com/',
   },
   {
     title: 'Dolly Parton',
     thumbnailUrl: 'https://pbs.twimg.com/profile_images/751068635297447936/9b2iGzv7_400x400.jpg',
+    soundcloud: '94792915',
     // url: 'https://dollyparton.com/',
   },
   {
     title: 'Flying Lotus',
     thumbnailUrl: 'https://pbs.twimg.com/profile_images/819979189231054848/DPZuZq3z_400x400.jpg',
+    soundcloud: '196380',
     // url: 'http://www.flying-lotus.com/',
   },
 ]
