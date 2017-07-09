fe48288eff7565560292873720f12f0dacb06691
willclarktech <willclarktech@users.noreply.github.com>
2017-07-09 14:31:20 +0200

    add artist thumbnails to seed data

--- packages/rockthatclub/lib/server/seed.js
+++ packages/rockthatclub/lib/server/seed.js
@@ -5,28 +5,29 @@ import Posts from '../modules/posts/collection'
 const seedData = [
   {
     title: 'Led Zeppelin',
+    thumbnailUrl: 'https://pbs.twimg.com/profile_images/2585954566/8ev9krfvkxb8nz00cg77_400x400.jpeg',
     // url: 'http://www.ledzeppelin.com/',
   },
   {
     title: 'Dolly Parton',
+    thumbnailUrl: 'https://pbs.twimg.com/profile_images/751068635297447936/9b2iGzv7_400x400.jpg',
     // url: 'https://dollyparton.com/',
   },
   {
     title: 'Flying Lotus',
+    thumbnailUrl: 'https://pbs.twimg.com/profile_images/819979189231054848/DPZuZq3z_400x400.jpg',
     // url: 'http://www.flying-lotus.com/',
   },
 ]
 
 Meteor.startup(function() {
-  console.info('// Creating dummy posts')
-  const demoUser = {
-    username: 'DemoUser',
-    email: 'demo@user.com',
-    profile: {
-      isDummy: true,
-    },
-  }
   if (!Users.find().fetch().length) {
+    console.info('// Creating demo user')
+    const demoUser = {
+      username: 'DemoUser',
+      email: 'demo@user.com',
+      isDummy: true,
+    }
     newMutation({
       collection: Users,
       document: demoUser,
@@ -35,6 +36,7 @@ Meteor.startup(function() {
   }
   const currentUser = Users.findOne()
   if (!Posts.find().fetch().length) {
+    console.info('// Creating dummy posts')
     seedData.forEach(post => newMutation({
       action: 'posts.new',
       collection: Posts,
