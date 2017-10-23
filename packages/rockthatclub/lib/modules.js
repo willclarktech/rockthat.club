4f40abad67d5774448b89e1792978896d8879feb
willclarktech <willclarktech@users.noreply.github.com>
2017-10-23 21:51:16 +0200

    :guitar: Add custom layout without newsletter

--- packages/rockthatclub/lib/modules.js
+++ packages/rockthatclub/lib/modules.js
@@ -12,11 +12,12 @@ import "./fragments.js";
 
 // custom components
 import "./components/CustomLogo.jsx";
-import "./components/CustomNewsletter.jsx";
+// import "./components/CustomNewsletter.jsx";
 import "./components/CustomPostsItem.jsx";
 import "./components/MyCustomPage.jsx";
 import "./components/MyPostsPage.jsx";
 import "./components/MyFooter.jsx";
+import "./components/MyLayout.jsx";
 
 // custom routes
 import "./routes.jsx";
